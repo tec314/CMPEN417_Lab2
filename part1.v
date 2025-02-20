833b5a1bd271ccb8e903eb2ea371ed424c250572
tec314 <102817416+tec314@users.noreply.github.com>
2025-02-20 12:21:47 -0500

    Initial repository upload

---
+++ part1.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 02/20/2025 12:11:57 PM
+// Design Name: 
+// Module Name: part1
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module part1(
+
+    );
+endmodule
